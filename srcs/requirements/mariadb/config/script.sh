333d5e73a634282d0d6ef2ede9d856ed4bf542f1
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2026-03-04 14:28:05 +0100

    mariadb change

--- srcs/requirements/mariadb/config/script.sh
+++ srcs/requirements/mariadb/config/script.sh
@@ -5,6 +5,11 @@ set -x
 sed -i 's/bind-address .*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i '/^#port/c\port = 3306' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+chmod 777 /var/run/mydqld/mysqld.sock
+socket=/var/lib/mysql/mysql.sock
+
+sudo service mysql start
+
 service mariadb start
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DBNAME ;"
@@ -12,6 +17,6 @@ mariadb -e "CREATE USER IF NOT EXISTS '$MARIADB_ADMIN_USER'@'%' IDENTIFIED BY '$
 mariadb -e "GRANT ALL PRIVILEGES ON $MARIADB_DBNAME.* TO '$MARIADB_ADMIN_USER'@'%' IDENTIFIED BY '$MARIADB_ROOT_PSWD' ;"
 mariadb -e "FLUSH PRIVILEGES;"
 
-service mariadb stop
+#service mariadb stop
 
 exec mysqld_safe
