e100876e87abf11ea469c795a2d84f860c6f92c0
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2026-03-04 14:33:58 +0100

    more debugging

--- srcs/requirements/mariadb/config/script.sh
+++ srcs/requirements/mariadb/config/script.sh
@@ -5,8 +5,6 @@ set -x
 sed -i 's/bind-address .*=.*/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i '/^#port/c\port = 3306' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-chmod 777 /var/run/mydqld/mysqld.sock
-
 service mariadb start
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DBNAME ;"
